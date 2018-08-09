dad43282c712e25a1633f261473bef9be7aa3b8c
John Elder <johne4196@gmail.com>
2018-08-09 12:40:15 -0700

    tweaked for heroku

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
             os.path.join(BASE_DIR, 'static'),
         ]
+
+django_heroku.settings(locals())
