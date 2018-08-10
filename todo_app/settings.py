a8bde14954e86105c10f97b7322e177594ca79dc
John Elder <johne4196@gmail.com>
2018-08-09 17:41:44 -0700

    adad

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
-import dj-database-url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
