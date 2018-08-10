569d2c227dd7af98ca9ead85b089119bde4f9d51
John Elder <johne4196@gmail.com>
2018-08-09 17:09:28 -0700

    stuff

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = []
 
